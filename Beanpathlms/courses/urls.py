880a63c453529bb63b7aaa357a889d0ecb54210e
Ricky Romanek <theromanek@gmail.com>
2025-04-13 22:52:54 -0500

    .gitignore

--- Beanpathlms/courses/urls.py
+++ Beanpathlms/courses/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = "course"
 
+# url patterns
 urlpatterns = [
     path("", views.IndexView.as_view(),name="index"),
     path("<int:pk>/course",views.CourseView.as_view(), name="course"),
